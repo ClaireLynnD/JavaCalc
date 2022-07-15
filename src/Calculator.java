0500c420c7eb02c68b0666faa17e5ce3b3086429
Claire <clairelynndickerson@gmail.com>
2022-07-15 12:13:00 -0500

    Basic Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    private double firstNum;
+    private double secondNum;
+    public double result;
+
+    public double getFirstNum() {
+        return firstNum;
+    }
+
+    public void setFirstNum(double firstNum) {
+        this.firstNum = firstNum;
+    }
+
+    public double getSecondNum() {
+        return secondNum;
+    }
+
+    public void setSecondNum(double secondNum) {
+        this.secondNum = secondNum;
+    }
+
+    public double add() {
+        result = firstNum + secondNum;
+        return result;
+    }
+
+    public double subtract() {
+        result = firstNum - secondNum;
+        return result;
+    }
+
+    public double multiply() {
+        result = firstNum * secondNum;
+        return result;
+    }
+
+    public double divide() {
+        try {
+            result = firstNum / secondNum;
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Undefined");
+        }
+        return result;
+    }
+
+    public double powerOf() {
+        result = Math.pow(firstNum, secondNum);
+        return result;
+    }
 }
+
--- tests/CalculatorTest.java
+++ tests/CalculatorTest.java
@@ -1,4 +1,56 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+
+    Calculator calculator;
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+        calculator.setFirstNum(7.0);
+        calculator.setSecondNum(5.0);
+    }
+
+    @Test
+    void firstNum() {
+        assertEquals(7.0, calculator.getFirstNum());
+    }
+
+    @Test
+    void secondNum() {
+        assertEquals(5.0, calculator.getSecondNum());
+    }
+
+    @Test
+    void add() {
+        assertEquals(12.0, calculator.add());
+    }
+
+    @Test
+    void subtract() {
+        assertEquals(2.0, calculator.subtract());
+    }
+
+    @Test
+    void multiply() {
+        assertEquals(35.0, calculator.multiply());
+    }
+
+    @Test
+    void divide() {
+        assertEquals(7.0/5.0, calculator.divide());
+    }
+
+    @Test
+    void divideByZero() {
+        calculator.setSecondNum(0);
+        assertEquals(calculator.getFirstNum()/0, calculator.divide());
+    }
+
+    @Test
+    void powerOf() {
+        assertEquals(16807.0, calculator.powerOf());
+    }
 }
